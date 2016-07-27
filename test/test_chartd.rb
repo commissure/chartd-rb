2ed6fcb7c10ae77f2752fddc4aaf4dbb5a6bf5d7
Max Lielje <max@maxlielje.co>
2016-07-27 13:12:12 +0200

    test: add tests for edge cases and custom min max values

--- test/test_chartd.rb
+++ test/test_chartd.rb
@@ -9,6 +9,8 @@ class ChartdTest < Test::Unit::TestCase
       [[0, 9, 9], 'A99'],
       [[0.944, 0.170, 0.201, 0.839, 0.284, 0.485, 0.154, 0.431, 0.119, 0.679], '9DG1MbCXAp'],
       [[0, -2, 9], 'LA9'],
+      [[0], 'A'],
+      [[], ''],
     ]
 
     testcases.each do |d, expected|
@@ -16,7 +18,14 @@ class ChartdTest < Test::Unit::TestCase
     end
   end
 
-  def test_empty_values
-    assert_equal '', Chartd.encode
+  def test_custom_min_max
+    testcases = {
+      'Ueo' => [[20, 30, 40], 0, 60],
+      'Ae9' => [[20, 30, 40], 20, 40],
+    }
+
+    testcases.each do |expected, data|
+      assert_equal expected, Chartd.encode(data[0], min: data[1], max: data[2])
+    end
   end
 end
